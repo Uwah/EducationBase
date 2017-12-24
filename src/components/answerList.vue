b3c691b31fd11732303c275a0752b9d75197ed9c
leung0601@163.com <leung0601@163.com>
2017-12-25 00:06:35 +0800

    更新提交

---
+++ src/components/answerList.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="answer-content" style="display: none;">
+        <div class="answer-head">
+            恭喜您，共答对<span class="right-answer">12</span>题，共<span class="total-answer">15</span>题
+        </div>
+        <div class="answer-item">
+            <h4 class="answer-title"><span class="answer-count">1、</span>噪声对人体哪个系统有害：</h4>
+            <ul class="answer-list">
+                <li class="active">A、心血管系统</li>
+                <li>B、消化系统</li>
+                <li>C、呼吸系统</li>
+            </ul>
+        </div>
+        <div class="answer-item">
+            <h4 class="answer-title error-answer"><span class="answer-count">2、</span>噪声对人体哪个系统有害：</h4>
+            <ul class="answer-list">
+                <li class="active">A、心血管系统</li>
+                <li>B、消化系统</li>
+                <li>C、呼吸系统</li>
+            </ul>
+        </div>
+        <div class="answer-item">
+            <h4 class="answer-title"><span class="answer-count">1、</span>噪声对人体哪个系统有害：</h4>
+            <ul class="answer-list">
+                <li class="active">A、心血管系统</li>
+                <li>B、消化系统</li>
+                <li>C、呼吸系统</li>
+            </ul>
+        </div>
+        <div class="answer-item">
+            <h4 class="answer-title error-answer"><span class="answer-count">2、</span>噪声对人体哪个系统有害：</h4>
+            <ul class="answer-list">
+                <li class="active">A、心血管系统</li>
+                <li>B、消化系统</li>
+                <li>C、呼吸系统</li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/winnerList.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="seasion-content" style="display: none;">
+        <div class="seasion-content-top">
+            <img src="" class="seasion-img" alt="">
+            <div class="seasion-info">
+                <span class="seasion-title">生活知识竞赛</span>
+                <span class="seasion-number">参与人数：756</span>
+            </div>
+        </div>
+        <div class="prize-group">
+            <h4 class="prize-name">获奖名单</h4>
+            <div class="student-group">
+                <h4 class="group-title">中学生组</h4>
+                <ul class="student name-list">
+                    <li>陈胤捷</li>
+                    <li>胡一天</li>
+                    <li>林静晓</li>
+                    <li>林静晓</li>
+                    <li>陈胤捷</li>
+                    <li>胡一天</li>
+                    <li>林静晓</li>
+                    <li>陈胤捷</li>
+                    <li>胡一天</li>
+                    <li>林静晓</li>
+                    <li>陈胤捷</li>
+                    <li>胡一天</li>
+                    <li>林静晓</li>
+                </ul>
+            </div>
+            <div class="adult-group">
+                <h4 class="group-title">成人组</h4>
+                <ul class="adult name-list">
+                    <li>张庭振</li>
+                    <li>赵梓帆</li>
+                    <li>殷恬歌</li>
+                    <li>张庭振</li>
+                    <li>赵梓帆</li>
+                    <li>殷恬歌</li>
+                    <li>张庭振</li>
+                    <li>赵梓帆</li>
+                    <li>殷恬歌</li>
+                    <li>张庭振</li>
+                    <li>赵梓帆</li>
+                    <li>殷恬歌</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
