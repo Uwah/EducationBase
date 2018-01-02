bef87448e2a12af4690ef2a2faea337e250906eb
v_yhhliang <v_yhhliang@wesure.cn>
2018-01-02 20:39:22 +0800

    弹框组件提交

---
+++ src/components/propModel.vue
@@ -0,0 +1,49 @@
+
+<template>
+<!-- 弹框组件 -->
+    <div class="sginup-bg" v-if="showStatus">
+        <div class="commit-success">{{propText}}</div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        showStatus: {
+            type: Boolean,
+            require: true,
+            default: false
+        },
+        propText: {
+            type: String,
+            require: true,
+            default: ''
+        }
+    }
+}
+</script>
+<style scoped>
+    .sginup-bg {
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        background-color: rgba(255, 255, 255, .6);
+        position: fixed;
+    }
+    .commit-success {
+        color: #3e3a39;
+        font-size: .54rem;
+        line-height: 1.52rem;
+        letter-spacing: 2px;    
+        height: 1.52rem;
+        max-width: 80%;
+        min-width: 4.5rem;
+        text-align: center;
+        background-color: rgba(255, 255, 255, .6);
+        box-shadow: 0px 0px 28px 0px rgba(0, 0, 0, 0.4);
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        position: absolute;
+    }
+</style>
