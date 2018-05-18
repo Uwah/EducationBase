f322f317825480e83f4b776c1c39cdd8fce0e31f
leung0601@163.com <leung0601@163.com>
2018-05-19 00:03:50 +0800

    幸运用户模块添加

---
+++ src/pages/luckyUserList.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- 幸运用户列表 -->
+    <div class="lucky-content">
+        <div class="lucky-head"></div>
+        <div class="lucky-content-list"></div>
+        <div class="lucky-footer"></div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'luckyUserList'
+}
+</script>
+<style>
+
+</style>
